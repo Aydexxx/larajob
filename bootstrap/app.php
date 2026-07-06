32b41acd03c80da6806d27bb6242ad4a8531b118
Aydexxx <bunyaminaydeniz0@gmail.com>
2026-07-06 14:33:15 +0300

    Trust Railway reverse proxy headers for correct scheme/host detection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,6 +23,14 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => CheckRole::class,
         ]);
+
+        $middleware->trustProxies(at: '*', headers:
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
